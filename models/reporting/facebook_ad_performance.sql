eaf8b6b7e7be00f634981446143fa3102a754038
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-01-10 10:52:09 -0600

    Update facebook_ad_performance.sql

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -30,31 +30,31 @@ act.complete_registration
 FROM {{ ref('facebook_performance_by_ad') }}
 LEFT JOIN 
     (SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity, ad_id,
-        COALESCE(SUM("7_d_click"),0) as complete_registration
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
     FROM {{ source('facebook_raw','ads_insights_actions') }}
     WHERE action_type = 'complete_registration'
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('week',date) as date, 'week' as date_granularity, ad_id,
-        COALESCE(SUM("7_d_click"),0) as complete_registration
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
     FROM {{ source('facebook_raw','ads_insights_actions') }}
     WHERE action_type = 'complete_registration'
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('month',date) as date, 'month' as date_granularity, ad_id,
-        COALESCE(SUM("7_d_click"),0) as complete_registration
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
     FROM {{ source('facebook_raw','ads_insights_actions') }}
     WHERE action_type = 'complete_registration'
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('quarter',date) as date, 'quarter' as date_granularity, ad_id,
-        COALESCE(SUM("7_d_click"),0) as complete_registration
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
     FROM {{ source('facebook_raw','ads_insights_actions') }}
     WHERE action_type = 'complete_registration'
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('year',date) as date, 'year' as date_granularity, ad_id,
-        COALESCE(SUM("7_d_click"),0) as complete_registration
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
     FROM {{ source('facebook_raw','ads_insights_actions') }}
     WHERE action_type = 'complete_registration'
     GROUP BY 1,2,3) act
