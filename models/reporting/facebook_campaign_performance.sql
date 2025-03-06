ccff9dd13150e52674767c6e855d4f5e2a05e728
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-06 20:29:20 +0100

    Create facebook_campaign_performance.sql

---
+++ models/reporting/facebook_campaign_performance.sql
@@ -0,0 +1,55 @@
+{{ config (
+    alias = target.database + '_facebook_campaign_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_effective_status,
+campaign_type_default,
+case 
+    when campaign_id = '6633504449611' and date_granularity = 'day' and date between '2024-12-27' and '2025-01-08' then 'Pre-Segmentation'
+    when campaign_id = '6633504449611' and date_granularity = 'day' and date between '2025-01-08' and '2025-02-03' then 'Post-Segmentation'
+else 'Other' end as period,
+date,
+date_granularity,
+spend,
+impressions,
+link_clicks,
+add_to_cart,
+purchases,
+revenue,
+ins.complete_registration as "complete_registration_7dc_1dv",
+act.complete_registration
+FROM {{ ref('facebook_performance_by_campaign') }} ins
+LEFT JOIN 
+    (SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity, campaign_id,
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
+    FROM {{ source('facebook_raw','campaigns_insights_actions') }}
+    WHERE action_type = 'complete_registration'
+    GROUP BY 1,2,3
+    UNION ALL
+    SELECT DATE_TRUNC('week',date) as date, 'week' as date_granularity, campaign_id,
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
+    FROM {{ source('facebook_raw','campaigns_insights_actions') }}
+    WHERE action_type = 'complete_registration'
+    GROUP BY 1,2,3
+    UNION ALL
+    SELECT DATE_TRUNC('month',date) as date, 'month' as date_granularity, campaign_id,
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
+    FROM {{ source('facebook_raw','campaigns_insights_actions') }}
+    WHERE action_type = 'complete_registration'
+    GROUP BY 1,2,3
+    UNION ALL
+    SELECT DATE_TRUNC('quarter',date) as date, 'quarter' as date_granularity, campaign_id,
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
+    FROM {{ source('facebook_raw','campaigns_insights_actions') }}
+    WHERE action_type = 'complete_registration'
+    GROUP BY 1,2,3
+    UNION ALL
+    SELECT DATE_TRUNC('year',date) as date, 'year' as date_granularity, campaign_id,
+        COALESCE(SUM("_7_d_click"),0) as complete_registration
+    FROM {{ source('facebook_raw','campaigns_insights_actions') }}
+    WHERE action_type = 'complete_registration'
+    GROUP BY 1,2,3) act
+USING(campaign_id,date,date_granularity)
