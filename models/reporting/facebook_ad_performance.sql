5db712dc2b8615ff9bed1f8c754650161968a029
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-03-24 10:29:05 +0100

    Update facebook_ad_performance.sql

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -31,9 +31,9 @@ add_to_cart,
 purchases,
 revenue,
 ins."offsite_conversion.fb_pixel_custom.shopifycompleteweddingpartyleadform" as "shopify_lead_7dc_1dv",
-act."offsite_conversion.fb_pixel_custom.shopifycompleteweddingpartyleadform" as "shopify_lead",
+act."shopify_lead",
 ins."offsite_conversion.fb_pixel_custom.shopifycreatorcheckout" as "shopify_checkout_7dc_1dv",
-act."offsite_conversion.fb_pixel_custom.shopifycreatorcheckout" as "shopify_checkout",
+act."shopify_checkout",
 ins.complete_registration as "complete_registration_7dc_1dv",
 act.complete_registration
 FROM {{ ref('facebook_performance_by_ad') }} ins
