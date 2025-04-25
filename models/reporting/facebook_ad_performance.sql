49997dc0dc4dcf180cebd4262ccfb0bf082e4a6a
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-04-25 14:56:50 +0200

    Update facebook_ad_performance.sql

--- models/reporting/facebook_ad_performance.sql
+++ models/reporting/facebook_ad_performance.sql
@@ -39,7 +39,9 @@ ins."offsite_conversion.fb_pixel_custom.shopifycreatorcheckout_value" as "shopif
 conv."shopify_checkout",
 conv_val."shopify_checkout_value",
 ins.complete_registration as "complete_registration_7dc_1dv",
-act.complete_registration
+act.complete_registration,
+ins."offsite_conversion.fb_pixel_custom.appointmentformsubmit" as "appointment_form_submit_7dc_1dv",
+conv.appointment_form_submit
 FROM {{ ref('facebook_performance_by_ad') }} ins
 LEFT JOIN 
     (SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity, ad_id,
@@ -70,31 +72,36 @@ USING(ad_id,date,date_granularity)
 LEFT JOIN 
     (SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity, ad_id,
         COALESCE(SUM(CASE WHEN action_type ~* 'shopifycompleteweddingpartyleadform' THEN "_7_d_click" ELSE 0 END),0) as shopify_lead,
-        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout
+        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout,
+        COALESCE(SUM(CASE WHEN action_type ~* 'appointmentformsubmit' THEN "_7_d_click" ELSE 0 END),0) as appointment_form_submit
     FROM {{ source('facebook_raw','ads_insights_conversions') }}
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('week',date) as date, 'week' as date_granularity, ad_id,
         COALESCE(SUM(CASE WHEN action_type ~* 'shopifycompleteweddingpartyleadform' THEN "_7_d_click" ELSE 0 END),0) as shopify_lead,
-        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout
+        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout,
+        COALESCE(SUM(CASE WHEN action_type ~* 'appointmentformsubmit' THEN "_7_d_click" ELSE 0 END),0) as appointment_form_submit
     FROM {{ source('facebook_raw','ads_insights_conversions') }}
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('month',date) as date, 'month' as date_granularity, ad_id,
         COALESCE(SUM(CASE WHEN action_type ~* 'shopifycompleteweddingpartyleadform' THEN "_7_d_click" ELSE 0 END),0) as shopify_lead,
-        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout
+        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout,
+        COALESCE(SUM(CASE WHEN action_type ~* 'appointmentformsubmit' THEN "_7_d_click" ELSE 0 END),0) as appointment_form_submit
     FROM {{ source('facebook_raw','ads_insights_conversions') }}
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('quarter',date) as date, 'quarter' as date_granularity, ad_id,
         COALESCE(SUM(CASE WHEN action_type ~* 'shopifycompleteweddingpartyleadform' THEN "_7_d_click" ELSE 0 END),0) as shopify_lead,
-        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout
+        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout,
+        COALESCE(SUM(CASE WHEN action_type ~* 'appointmentformsubmit' THEN "_7_d_click" ELSE 0 END),0) as appointment_form_submit
     FROM {{ source('facebook_raw','ads_insights_conversions') }}
     GROUP BY 1,2,3
     UNION ALL
     SELECT DATE_TRUNC('year',date) as date, 'year' as date_granularity, ad_id,
         COALESCE(SUM(CASE WHEN action_type ~* 'shopifycompleteweddingpartyleadform' THEN "_7_d_click" ELSE 0 END),0) as shopify_lead,
-        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout
+        COALESCE(SUM(CASE WHEN action_type ~* 'shopifycreatorcheckout' THEN "_7_d_click" ELSE 0 END),0) as shopify_checkout,
+        COALESCE(SUM(CASE WHEN action_type ~* 'appointmentformsubmit' THEN "_7_d_click" ELSE 0 END),0) as appointment_form_submit
     FROM {{ source('facebook_raw','ads_insights_conversions') }}
     GROUP BY 1,2,3) conv
 USING(ad_id,date,date_granularity)
